b8260919514f3b7aadf7f1606e31fe0b2bd008d8
Amir Mehmood <amehmood@broadsoft.com>
2017-05-10 11:36:18 -0400

    adds the receive webhook

--- server.js
+++ server.js
@@ -1,4 +1,9 @@
 var express = require('express');
+var bodyParser = require('body-parser')
 var app = express();
+app.use(bodyParser.json())
 app.use(express.static(__dirname + '/dist'));
 app.listen(process.env.PORT || 80);
+app.post('/receive', function (req, res) {
+  res.send(req.body);
+})
