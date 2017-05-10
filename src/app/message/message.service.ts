36ec6b43118aeaa9fc139990d80744652309358d
Amir Mehmood <amehmood@broadsoft.com>
2017-05-10 10:08:22 -0400

    adds heroku configurations

--- src/app/message/message.service.ts
+++ src/app/message/message.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
+import { Headers, RequestOptions } from '@angular/http';
 
 import 'rxjs/add/operator/toPromise';
 
@@ -7,15 +8,18 @@ import { environment } from '../../environments/environment';
 
 @Injectable()
 export class MessageService {
-  private _apiUrl =  `${environment.host}:${environment.port}/api/v1/messages`;
+  // private _apiUrl =  `${environment.host}:${environment.port}/api/v1/messages`;
+  private _apiUrl =  ` https://young-basin-29738.herokuapp.com/sendMessage/+3124834811`;
 
   constructor(
     private _http: Http,
   ) { }
 
   sendMessage(message: string): Promise<any> {
+    let headers = new Headers({ 'Content-Type': 'application/json' });
+    let options = new RequestOptions({ headers: headers });
     return this._http
-      .post(this._apiUrl, {message: message})
+      .post(this._apiUrl, {message}, options)
       .toPromise()
       .then((response: any) => {
         response = response.json();
