1a1e41861cc9a55e1d2b1d4f0f9317fa6bf0878d
Amir Mehmood <amehmood@broadsoft.com>
2017-05-10 13:50:13 -0400

    fixes the conflict between express and angular routes

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser')
 var app = express();
 var http = require('http').Server(app);
+var fs = require("fs");
 // var io = require('socket.io')(http, {origins:'*'});
 var io = require('socket.io')(http);
 
@@ -18,4 +19,12 @@ app.listen(process.env.PORT || 80);
 app.post('/receive', function (req, res) {
   io.emit('message', {type:'new-message', text: req.body});
   console.log(JSON.stringify(req.body));
+})
+
+app.get(/^\/(.*)(?:\/|$)/, function (req, res) {
+  fs.readFile("./dist/index.html", {
+    "encoding": "utf8"
+  }, (err, data) => {
+    res.send(data)
+  });
 })
