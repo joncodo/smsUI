966ce45c1ac62572c753dbbb6aac362ba71ceae5
Amir Mehmood <amehmood@broadsoft.com>
2017-05-10 15:12:20 -0400

    adding cors

--- server.js
+++ server.js
@@ -5,12 +5,24 @@ var http = require('http').Server(app);
 var fs = require("fs");
 // var io = require('socket.io')(http, {origins:'*'});
 var io = require('socket.io')(http);
+var cors = require('cors');
 
 io.on('connection', (socket) => {
   console.log('user connected');
   socket.on('disconnect', function(){
     console.log('user disconnected');
   });
+});
+
+app.use(cors({
+  "origin": "*",
+  "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+  "preflightContinue": true,
+  "credentials": true
+}));
+
+app.options('/*', function(req, res) {
+res.send(200, 'CHECKOUT,CONNECT,COPY,DELETE,GET,HEAD,LOCK,M-SEARCH,MERGE,MKACTIVITY,MKCALENDAR,MKCOL,MOVE,NOTIFY,PATCH,POST,PROPFIND,PROPPATCH,PURGE,PUT,REPORT,SEARCH,SUBSCRIBE,TRACE,UNLOCK,UNSUBSCRIBE');
 });
 
 app.use(bodyParser.json())
